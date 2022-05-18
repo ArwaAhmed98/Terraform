6ecabee2cc76ac0ee889582d119c9b37461273b5
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-05-18 23:15:12 +0200

    edit infrastructure

--- ansible4EC2.sh
+++ ansible4EC2.sh
@@ -1,5 +1,11 @@
-sudo apt-get upgrade 
-sudo  apt-get install python3-pip -y
+
+sudo add-apt-repository universe
+sudo apt-get update
+sudo  apt install python3-pip -y
 sudo pip3 install boto3
-sudo python get-pip.py
-sudo python -m pip install ansible
+sudo python3 get-pip.py
+sudo python3 -m pip install ansible
+
+
+
+ssh -o ProxyCommand="ssh -W %h:%p -q publicec2" -i ~/.ssh/pub-key.pem ubuntu@10.0.3.216
--- network/privatertb.tf
+++ network/privatertb.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "privatertb" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.itivpc-igw.id
+    gateway_id = aws_nat_gateway.nat.id
   }
 
   tags = {
