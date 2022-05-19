d1ee71f5dcac10dfb25ec16c5c2901c28590ff3b
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-05-19 20:53:00 +0200

    add-another-ec2

--- publicec2.tf
+++ publicec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "pubec2" {
 
 ###################################################################################################
 
-resource "aws_instance" "pubec2" {
+resource "aws_instance" "pubec22" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   subnet_id = module.network.pub_sub2_id
