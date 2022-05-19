78ea819a94b3ff8dea7f5f64ee5b420198eaf777
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-05-19 20:49:18 +0200

    add-another-ec2

--- publicec2.tf
+++ publicec2.tf
@@ -9,3 +9,20 @@ resource "aws_instance" "pubec2" {
     Name = "publicec2"
   }
 }
+
+
+
+
+###################################################################################################
+
+resource "aws_instance" "pubec2" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  subnet_id = module.network.pub_sub2_id
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.publicsg.id}"] #attach ec2 to public secuirty group
+  key_name = "pub-key"
+  tags = {
+    Name = "publicec22"
+  }
+}
