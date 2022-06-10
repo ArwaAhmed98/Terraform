5e213928f231364e8401fe443bb3f763e8591cb6
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-06-10 11:29:45 +0200

    edit name of itisthreee

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
   # region = var.region
-  # profile="dev" # deal with the dev credentials in ~/.aws/credentials
+  profile="dev" # deal with the dev credentials in ~/.aws/credentials
 }
