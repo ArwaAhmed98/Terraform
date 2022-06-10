1046ca4d6bdadb1c0a01db7132074ccddfbb96bb
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-06-10 10:55:11 +0200

    edit prvatec2.tf

--- Terraform/privatec2.tf
+++ Terraform/privatec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "privateec2" {
   instance_type = "t2.micro"
   subnet_id = module.network.priv_sub1_id
   associate_public_ip_address = false
-  vpc_security_group_ids = ["${aws_security_group.privatesg.id}"] #attach ec2 to public secuirty group
+  vpc_security_group_ids = ["${aws_security_group.privatesg.id}",""] #attach ec2 to public secuirty group
   key_name = "pub-key"
   tags = {
     Name = "privatec2"
